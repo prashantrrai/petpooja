6f8a7bd7f25c4b46f5e234a934a0d8d5e447aa99
Prashant Rai <19ce073@gardividyapith.ac.in>
2024-09-08 14:21:29 +0530

    Feat: Created Seat Booking Page and Updated Home Page

--- Client/src/app/pages/home/home.component.ts
+++ Client/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- Client/src/app/pages/train-seat-selection/train-seat-selection.component.ts
+++ Client/src/app/pages/train-seat-selection/train-seat-selection.component.ts
@@ -1,12 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-train-seat-selection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './train-seat-selection.component.html',
   styleUrl: './train-seat-selection.component.css'
 })
 export class TrainSeatSelectionComponent {
+  isTrainMoving: boolean = true;
+  showModal: boolean = false;
 
+  seatsA = [
+    { label: 'A1', status: 'available' },
+    { label: 'A2', status: 'available' },
+    { label: 'A3', status: 'available' },
+    { label: 'A4', status: 'available' },
+    { label: 'A5', status: 'unavailable' },
+    { label: 'A6', status: 'available' }
+  ];
+
+  seatsB = [
+    { label: 'B1', status: 'available' },
+    { label: 'B2', status: 'available' },
+    { label: 'B3', status: 'available' },
+    { label: 'B4', status: 'available' },
+    { label: 'B5', status: 'unavailable' },
+    { label: 'B6', status: 'available' }
+  ];
+
+  pauseTrain() {
+    this.isTrainMoving = false;
+  }
+
+  resumeTrain() {
+    this.isTrainMoving = true;
+  }
+
+  selectSeat(seat: any) {
+    if (this.isTrainMoving || seat.status === 'unavailable') return;
+    seat.status = seat.status === 'selected' ? 'available' : 'selected';
+  }
+
+  // confirmSelection() {
+  //   this.showModal = true;
+  // }
+
+  // onConfirm(result: boolean) {
+  //   this.showModal = false;
+  //   if (result) {
+  //     console.log('Booking confirmed');
+  //   } else {
+  //     console.log('Booking cancelled');
+  //   }
+  // }
+
+  onConfirm() {
+    console.log('Booking confirmed');
+  }
 }
